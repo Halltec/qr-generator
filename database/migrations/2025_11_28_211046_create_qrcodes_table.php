ac016e1a33e8fee87c87b53ae9610b0ed8d4aac6
oaguillon <aguillon_@hotmail.com>
2025-11-28 16:54:48 -0500

    add Qrcode migration fields

--- database/migrations/2025_11_28_211046_create_qrcodes_table.php
+++ database/migrations/2025_11_28_211046_create_qrcodes_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('qrcodes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('url')->nullable();
+            $table->longText('qr_code')->nullable();
+            $table->longText('description')->nullable();
             $table->timestamps();
         });
     }
