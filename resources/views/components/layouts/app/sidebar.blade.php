332eecc894fd347d52eecac3a34c6d6b97d53e55
oaguillon <aguillon_@hotmail.com>
2025-12-01 09:00:23 -0500

    add link into sidebar menu

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -14,6 +14,7 @@
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+                    <flux:navlist.item icon="device-phone-mobile" :href="route('qrcode.index')" :current="request()->routeIs('qrcode.*')" wire:navigate>{{ __('QR Codes') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
